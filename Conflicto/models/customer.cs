16a886190e178e2da379e5b4faf81e4371ec7a5a
CodigCar <carlos.castilla009@gmail.com>
2020-05-19 18:51:09 -0500

    add_customer

---
+++ Conflicto/models/customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conflicto.models
+{
+    public class customer
+    {
+        public int Id { get; set; }
+    }
+}
