ab74f070a8017ea00416506b45e8287b3056af5a
CodigCar <carlos.castilla009@gmail.com>
2020-05-19 18:56:30 -0500

    add_pet

---
+++ Conflicto/models/Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conflicto.models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
