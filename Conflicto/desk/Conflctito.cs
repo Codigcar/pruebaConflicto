6aef916d106e064e3e49d8193355c671daf3edf9
Joaquin <joaquinegocheagaramos@gmail.com>
2020-05-19 23:18:02 -0500

    change

---
+++ Conflicto/desk/Conflctito.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conflicto.desk
+{
+    public class Conflctito
+    {
+        String chino { get; set; }
+    }
+}
